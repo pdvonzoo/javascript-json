afdb204f83c83be35d21fcff98c18bcb71c5d24d
chany <lyhy0310@gmail.com>
2018-04-13 23:26:58 +0900

    _isOBjString testCode 하나씩 분리하며 수정

--- ArrayParser/Test/_isObjString.test.js
+++ ArrayParser/Test/_isObjString.test.js
@@ -1,16 +1,25 @@
 const {describe, test, equal, expect} = require('../testUtil')
 const _isObjString = require('../modules/_isObjString')
 
+equal(_isObjString('{{}'), true)
+     
 
 describe('_isObjString Test', ()=>{
-    test('정상 값 test',()=>{
+    test('기본 {} 테스트',()=>{
         equal(_isObjString('{}'), true)
-        equal(_isObjString('{{}'), true)
+    })
+    test('중첩 obj {{}} 테스트',()=>{
         equal(_isObjString('{{2}}'), true)
     })
-    test('오류값 잘못된 값',()=>{
+    test('첫번째 클로즈드에서는 검출되지 않는 에러값 테스트',()=>{
+        equal(_isObjString('{{}'), true)
+    })
+    test('오브젝트가 아닌 값 테스트',()=>{
         equal(_isObjString('asd[]das'),false)
+    })
+    test('오브젝트와 값이 붙어있는 에러값 테스트',()=>{
         equal(_isObjString('{}a'), false)
     })
+  
 })
 
