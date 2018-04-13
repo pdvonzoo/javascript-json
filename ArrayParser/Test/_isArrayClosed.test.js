adaa6cd7138dc06d9cd1d4b6819d286526bb676d
chany <lyhy0310@gmail.com>
2018-04-13 23:31:47 +0900

    _isArraytString  testCode 하나씩으로  분리하여 수정

--- ArrayParser/Test/_isArrayClosed.test.js
+++ ArrayParser/Test/_isArrayClosed.test.js
@@ -1,15 +1,23 @@
 const {describe, test, equal, expect} = require('../testUtil')
 const _isArrayClosed = require('../modules/_isArrayClosed')
-
+  
 describe('is Array String Test', ()=>{
-    test('정상값[]',()=>{
+    test('정상값 [] 테스트',()=>{
         equal(_isArrayClosed('[]'), true)
-        equal(_isArrayClosed(' [[] '), true)
+    })
+    test('중첩 배열 테스트 [[]]',()=>{
         equal(_isArrayClosed('[[2]]'), true)
     })
-    test('오류값 잘못된 값',()=>{
+    test('첫번째 클로즈드에서는 검출되지 않는 에러값 테스트 [[]',()=>{
+        equal(_isArrayClosed('[[]'), true)
+    })
+    test('배열과 섞여 있는 값 테스트',()=>{
         equal(_isArrayClosed('asd[]das'),false)
+    })
+    test('배열이 아닌 값 테스트 1',()=>{
         equal(_isArrayClosed('{}'), false)
+    })
+    test('배열이 아닌 값 테스트 2',()=>{
         equal(_isArrayClosed('123'), false)
     })
 })
