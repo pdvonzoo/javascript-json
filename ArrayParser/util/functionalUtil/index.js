ced3f8b30cf07a0cdafe27e1210d2eb318748727
chany <lyhy0310@gmail.com>
2018-04-20 20:25:55 +0900

    [convention] functional util arrow function으로 통일

--- ArrayParser/util/functionalUtil/index.js
+++ ArrayParser/util/functionalUtil/index.js
@@ -21,13 +21,13 @@ const filter = (list, predicate) => {
     return new_list;
 }
 
-function curry(fn){
+const curry = (fn)=>{
     return function(a,b){
          return arguments.length===2 ? fn(a,b) :function(b){return fn(a,b);}
     }
 }
 
-const curryR = function(fn){
+const curryR = (fn)=>{
     return function(a,b){
         return arguments.length===2 ? fn(a,b) :function(b){return fn(b,a);}
    }
