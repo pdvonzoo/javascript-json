57af5e610daf92e451ea51d442f024d5f5203816
HYEON(win) <antaehyeon@live.co.kr>
2018-05-23 19:17:10 +0900

    Refactor: arrayParser 테스트파일 추가

---
+++ arrayParser.test.js
@@ -0,0 +1,43 @@
+/*
+    - divideString()
+    - recursionCase(mergeData)
+    - createObject()
+    - createCurlyObject()
+    - setObjectData(mode, inputData)
+    - typeDecision(inputData)
+    - checkCorrectString(inputData)
+    - removeSpace(inputData)
+    - removeFirstParenthesis(inputData)
+    - checkType(params)
+    - getResult()
+
+    메서드 크기로 구분
+    00
+    - recursionCase(mergeData)
+    - getResult()
+    01
+    - divideString()
+    - setObjectData(mode, inputData)
+    - removeSpace(inputData)
+    - removeFirstParenthesis(inputData)
+    02
+    - createCurlyObject()
+    - typeDecision(inputData)
+    - checkCorrectString(inputData)
+    - checkType(params)
+    03
+    - createObject()
+
+    - given 어떤 상황이 들어간다
+    - when 어떻게 동작한다
+    - then 동작한 결과가 어떠해야 한다
+*/
+
+
+
+
+
+
+
+
+
