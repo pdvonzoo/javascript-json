8e0434536f41c076633808c6484034e693e42e38
imdonguk <com8157@naver.com>
2018-05-23 20:25:07 +0900

    Refactoring : lexer클래스 따로 파일에 분리

---
+++ src/lexer.js
@@ -0,0 +1,42 @@
+const {syntaxError} = require('./error.js');
+exports.lexer = (value, key) => new lexer(value,key);
+
+class lexer{
+    constructor(value, key){
+        this.value = value;
+        this.key = key;
+    }
+    typeCheck(){
+		if(this.value[0] === "[")return "array";
+		if(this.value[0] === "{")return "object";
+		if(!isNaN(+this.value))return "number";
+		const dic = {
+			true : "boolean",
+			false : "boolean",
+			undefined : "undefined",
+			null : "null"
+		}
+		for(let key in dic){
+			if( key === this.value ) return dic[key];
+        }
+        syntaxError(this.value);
+		return "string";
+    }
+    valueCheck(){
+        const type = this.typeCheck();
+        if(type === "array")return "object Array";
+        if(type === "object")return "object Object";
+        return this.value;
+    }
+    keyCheck(){
+        return this.key;
+    }
+    get getLexer(){
+        let dic = {};
+        dic.type = this.typeCheck();
+        dic.value = this.valueCheck();
+        if(!!this.key) dic.key = this.keyCheck();
+        dic.child = [];
+        return dic;
+    }
+}
