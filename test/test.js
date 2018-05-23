b7cb31be59856247fa30fdda1992f561f13a87c4
imdonguk <com8157@naver.com>
2018-05-23 20:29:55 +0900

    modify : expect클래스의 equal, arrayEqual, objectEqual 메소드 수정

---
+++ test/test.js
@@ -0,0 +1,53 @@
+
+exports.test = test;
+exports.expect = (result) => new expect(result);
+
+function test(testMessage, testFunc){
+	console.log(testMessage + " : " + testFunc());
+}
+
+class expect{
+	
+	constructor(result){
+		this.result = result;
+	}
+
+	equal(target, expect){
+		if( Object.prototype.toString.call(target) === "[object Array]" ){
+			return this.arrayEqual(target, expect);
+		}
+		if( Object.prototype.toString.call(target) === "[object Object]" ){
+			return this.objectEqual(target, expect);
+		}
+		return target === expect;
+	}
+
+	arrayEqual(target, expect){
+		return target.every( (v,i) => {
+			if( typeof v === "object" )return this.equal(v,expect[i]);
+			return v === expect[i];
+		})
+	}
+
+	objectEqual(target, expect){
+		const targetLength = Object.keys(target).length;
+		const expectLength = Object.keys(expect).length;
+		if(targetLength !== expectLength)return false;
+		for( let key in target){
+			if( !expect.hasOwnProperty(key) )return false;
+			if(target[key] === expect[key]) continue;
+			if( typeof target[key] === "object" )this.equal(target[key],expect[key]);
+		}
+		return true;
+	}
+
+	toBe(answer){
+		const targetMessage = `targetValue is ${JSON.stringify(this.result)}`;
+		const expectMessage = `expectValue is ${JSON.stringify(answer)}`;
+		if( this.equal(answer, this.result) )return 'OK';
+		return `FAIL (${targetMessage}, ${expectMessage})`;
+	}
+
+}
+
+console.log(new expect().equal({}, {}))
