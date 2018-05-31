e581ebdddd6c6e2a7e09d4d204fa79e5fca89041
Log91 <peter1636@naver.com>
2018-05-31 12:01:24 +0900

    Update : 여섯 가지 테스트코드 추가
    추가된 테스트코드
    중괄호, 대괄호 개수 테스트
    문자열 'Null'과 'Boolean'을 각각 Null, Boolean 타입으로 변경하는 테스트
    structure의 parser함수 테스트
    tokenize함수 테스트

--- src/test.js
+++ src/test.js
@@ -4,15 +4,19 @@ const { arrayParser } = require('./parser');
 const { jsonParser } = require('./parser');
 const FORMAT = {
   ARRAY: function (testValue, expectValue, errorArray) {
-    const errorMessage = `
-  FAIL 
-  (targetValue : ${testValue}, expectValue : ${expectValue})
-  
+    const errorMessage = `FAIL
   상세내용
   검출된 에러 : ${errorArray.length}개
   ${errorArray.join('\r')}
   `
     return errorMessage;
+  },
+  TYPE_DISCORD: function (typeOfExpect, typeOfTest) {
+    const errorMessage = `
+    기대값과 테스트값의 타입이 불일치 합니다
+    expect : ${typeOfExpect}
+    test : ${typeOfTest}
+    `
   }
 
 }
@@ -23,13 +27,35 @@ exports.test = function (testCase, valueFunction) {
 exports.expect = function (expectValue) {
   const fn = {
     toBe(testValue) {
-      let errorArray = [];
-      comparingArray(expectValue, testValue, errorArray);
-      const completedMessage = !errorArray.length ?
-        'OK' : FORMAT.ARRAY(testValue, expectValue, errorArray)
-        ;
+      const typeOfExpect = toString.call(expectValue);
+      const typeOfTest = toString.call(testValue);
+      if (typeOfExpect !== typeOfTest) FORMAT.TYPE_DISCORD(typeOfExpect, typeOfTest);
+      let completedMessage = '';
+      if (typeOfExpect === '[object Array]') completedMessage = compareArrayType(expectValue, testValue);
+      else if (typeOfExpect === '[object Object]') completedMessage = compareObjectType(expectValue, testValue);
+      else completedMessage = (testValue === expectValue) ? 'OK'
+        : `FAIL targetValue : ${testValue} expectValue : ${expectValue}`;
       return completedMessage;
     }
+  }
+  function compareArrayType(expectValue, testValue) {
+    let errorArray = [];
+    comparingArray(expectValue, testValue, errorArray);
+    const completedMessage = !errorArray.length ?
+      'OK' : FORMAT.ARRAY(testValue, expectValue, errorArray);
+    return completedMessage;
+  }
+  function compareObjectType(expect, test) {
+    let errorArray = [];
+    let expectKey = Object.keys(expect);
+    let testKey = Object.keys(test);
+    let expectValue = Object.values(expect);
+    let testValue = Object.values(test);
+    comparingArray(expectKey, testKey, errorArray);
+    comparingArray(expectValue, testValue, errorArray);
+    const completedMessage = !errorArray.length ?
+      'OK' : FORMAT.ARRAY(testValue, expectValue, errorArray);
+    return completedMessage;
   }
   return fn;
 }
@@ -46,4 +72,16 @@ function comparingArray(expectCode, testCode, errorArray) {
     expectValue = ${cv}
     targetValue = ${testCode[idx]}`);
   })
+}
+function compareObjectType(expect, test) {
+  let errorArray = [];
+  let expectKey = Object.keys(expect);
+  let testKey = Object.keys(test);
+  let expectValue = Object.values(expect);
+  let testValue = Object.values(test);
+  comparingArray(expectKey, testKey, errorArray);
+  comparingArray(expectValue, testValue, errorArray);
+  const completedMessage = !errorArray.length ?
+    'OK' : FORMAT.ARRAY(testValue, expectValue, errorArray);
+  return completedMessage;
 }
--- src/testCase.js
+++ src/testCase.js
@@ -1,13 +1,54 @@
 const { DataStructure } = require('./structure');
 const { Syntax } = require('./checker');
+const { ErrorMessage } = require('./checker')
 const { test } = require('./test');
 const { expect } = require('./test');
 const { ArrayParser } = require('./parser');
 const { JsonParser } = require('./parser');
+const { Tokenizer } = require('./tokenizer');
 
-test('배열화 과정을 거친 값과 값이 같다', function () {
+const syntax = new Syntax();
+test("Check Pair SquareBracket", function () {
+  const testCode = syntax.isPairBracket('[]]');
+  const expectCode = true;
+  return expect(expectCode).toBe(testCode);
+});
+
+test("Check Pair CurlyBracket", function () {
+  const testCode = syntax.isPairBracket('{{{}}}');
+  const expectCode = true;
+  return expect(expectCode).toBe(testCode);
+});
+
+test("Null이나 Boolean 문자열을 Null Boolean Type으로 변경", function () {
+  const testCode = syntax.changeToNullAndBoolean('null');
+  const expectCode = null;
+  return expect(expectCode).toBe(testCode);
+});
+
+test("Check Pair Quote", function () {
+  const testCode = syntax.checkPairQuote("''");
+  const expectCode = true;
+  return expect(expectCode).toBe(testCode);
+});
+
+test('Array structure.parser', function () {
   const structure = new DataStructure(new Syntax(Syntax.errorMessage), ArrayParser, JsonParser);
   const testCode = structure.parser("[[1,2,2,4],'b',{a:null,b:true,c:false}]");
   const expectCode = [[1, 2, 2, 4], 'b', { a: null, b: true, c: false }];
+  return expect(expectCode).toBe(testCode);
+});
+
+test("Tokenize과정을 거친 배열", function () {
+  const tokenizer = new Tokenizer(new Syntax(Syntax.errorMessage));
+  const testCode = tokenizer.tokenize(['whale', 123, false]);
+  const expectCode = {
+    type: 'array',
+    value: 'ArrayObject',
+    child:
+      [{ type: 'string', value: 'whale', child: [] },
+      { type: 'number', value: 123, child: [] },
+      { type: 'string', value: false, child: [] }]
+  }
   return expect(expectCode).toBe(testCode);
 });
