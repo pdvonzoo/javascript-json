621d48f9d896f9b36bbda394b59050925e5aa361
Log91 <peter1636@naver.com>
2018-06-09 15:53:50 +0900

    update: 파싱한 데이터의 타입 개수를 출력하는 기능 추가
    tokenize할 때 최종적으로 모든 값들을 순환하기 때문에 파싱할 때마다 카운트값을 담아줄 객체를 넘겨주고 객체의 값을 변경하는 방식으로 카운트 한다

--- src/arrayParser.js
+++ src/arrayParser.js
@@ -6,8 +6,9 @@ const jsonParser = require('./parser').JsonParser;
 
 class ArrayParser {
   constructor() {
+    this.countByType = { object: 0, array: 0, boolean: 0, null: 0, number: 0, string: 0 };
     this.syntaxChecker = new Syntax();
-    this.tokenizer = new Tokenizer(this.syntaxChecker);
+    this.tokenizer = new Tokenizer(this.syntaxChecker, this.countByType);
     this.dataStructure = new DataStructure(this.syntaxChecker, arrayParser, jsonParser);
   }
   parse(str) {
@@ -16,9 +17,20 @@ class ArrayParser {
     const fixedArray = this.tokenizer.tokenize(arrayed);
     return fixedArray;
   }
+  countTypeFormatting(countByTypeObject) {
+    const key = Object.keys(countByTypeObject);
+    const reduced = key.reduce((ac, cv, idx) => {
+      ac += `${cv}타입 : ${countByTypeObject[cv]}개, `
+      if (idx === key.length - 1) return ac.slice(0, -2);
+      return ac;
+    }, ``)
+    return reduced;
+  }
 }
 
 const str = "['wef', [789, {a:'b',c:'d',e:[{a:'b',d:{a:'b'}}]},null,true,'a', ['a',[1,32,3],2], 2],false, 1,2]";
 const ap = new ArrayParser();
 const result = ap.parse(str);
+const countByType = ap.countByType;
 console.log(JSON.stringify(result, null, 2));
+console.log(ap.countTypeFormatting(ap.countByType));
