b9ef2615be226503b770e8e305d1e60f07e1198d
HYEON(mac) <antaehyeon@live.co.kr>
2018-06-19 17:03:13 +0900

    Refactor: arrayParser test case 1개 추가

--- test/arrayParser.test.js
+++ test/arrayParser.test.js
@@ -1,65 +1,4 @@
-/*
-    - divideString()
-        - String 을 한글자씩 쪼개서 문자배열로 반환하는 메서드
-        - IN : 문자열 (String)
-        - OUT : 문자배열 (String[])
-    - recursionCase(mergeData)
-        - 재귀로 진입하는 메서드 (새로운 클래스 생성)
-        - IN : mergeData (String)
-        - OUT : mergeData (Object)
-    - createObject()
-        - 본격적인 메서드
-        - Object를 만드는 메서드이다
-        - recursionCase 메서드와 typeDecesion 메서드가 사용됨 (너무 메서드를 사용하는게 없는 듯 함)
-        - IN : ?
-        - OUT : ?
-    - createCurlyObject()
-        - {} 괄호를 만들 때, 사용하는 메서드인데
-        - createObject랑 비슷한 부분이 많다
-        - 비슷하지만 메서드로 묶기에는 너무 달라서
-        - 비슷하면서도 다른 메서드 (개인적으로 아쉬운 부분)
-        - IN : ?
-        - OUT : ?
-    - setObjectData(mode, inputData)
-        - Object 일 경우, KEY-VALUE 변수에 데이터를 지정해주는 메서드
-        - IN : mode ("KEY" or "VALUE"), inputData + resultObject
-        - OUT : resultObject
-    - typeDecision(inputData)
-        - 타입을 결정해주는 메서드
-    - checkCorrectString(inputData)
-        - 정확한 데이터인지 확인하는 메서드
-    - removeSpace(inputData)
-        - 공백을 제거하는 메서드
-    - removeFirstParenthesis(inputData)
-        - 첫번째 괄호를 제거하는 메서드
-    - checkType(params)
-        - 타입을 확인하는 메서드
-        - typeDecesion 메서드에서 사용된다
-    - getResult()
-        - 결과를 출력하는 메서드
 
-
-    메서드 크기로 구분
-    00
-    - recursionCase(mergeData)
-    - getResult()
-    01
-    - divideString()
-    - setObjectData(mode, inputData)
-    - removeSpace(inputData)
-    - removeFirstParenthesis(inputData)
-    02
-    - createCurlyObject()
-    - typeDecision(inputData)
-    - checkCorrectString(inputData)
-    - checkType(params)
-    03
-    - createObject()
-
-    - given 어떤 상황이 들어간다
-    - when 어떻게 동작한다
-    - then 동작한 결과가 어떠해야 한다
-*/
 const {test} = require('./test');
 const {expect} = require('./expect');
 const {ArrayParser} = require('../function/arrayParser');
@@ -72,6 +11,14 @@ test("타입을 올바르게 결정하는지 확인한다", () => {
     const testData = "[123, [22], 33]";
     const testResult = arrayParser.getResult(testData);
     const answer = {"type":"Array","child":[{"type":"Number","value":"123","child":[]},{"type":"Array","child":[{"type":"Number","value":"22","child":[]}]},{"type":"Number","value":"33","child":[]}]};
+
+    expect(answer).toBe(testResult);
+});
+
+test("타입을 올바르게 분석하는지 확인한다", () => {
+    const testData = "[123, [22], 33]";
+    const testResult = arrayParser.getStats(testData);
+    const answer = "타입 갯수를 분석하여 결과를 출력합니다\narray: 2\nstring: 0\nnull: 0\nboolean: 0\nobject: 0\nnumber: 3\n";
 
     expect(answer).toBe(testResult);
 });
