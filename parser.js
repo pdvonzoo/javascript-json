4d9c00d3e3ca3d822bf9430d7efbc7e156861fce
feanar729 <feanar729@gmail.com>
2018-06-22 15:02:58 +0900

    test & 미완성: 재 제작 테스트 코드 실행 보완 및 refactoring 필요

--- parser.js
+++ parser.js
@@ -1,74 +1,75 @@
-class test {
-  constructor(child = []) {
-    this.child = child;
-  }
+function getFilterData(data) {
+  const delOutsideArr = data.substring(1, data.length - 1);
 
-  filterData(data) {
-    const delOutsideArr = data.substring(1, data.length - 1);
+  let splitData = delOutsideArr.split('');
+  let filteringData = splitData.filter(element => {
+    return element !== ' ';
+  }).reduce((accData, index) => {
+    return accData + index;
+  });
+  return filteringData;
+}
 
-    let splitData = delOutsideArr.split('');
+function test(arr) {
+  let arrTest = [];
+  console.log(arr)
+  const test = arr.reduce((accVal, value, index) => {
+    if (value.match(',')) console.log('"," 임', value)
+    if (value.match(/[0-9]/)) console.log(accVal, value);
+    if (value.match(']')) console.log('"]" 임', value, index)
+  });
+}
 
-    let filteringData = splitData.filter(element => {
-      return element !== ' ' && element !== ',';
-    }).reduce((accData, index) => {
-      return accData + index;
-    });
-    return filteringData;
-  }
+function parser(str) {
+  const filteringData = getFilterData(str);
+  const splitData = filteringData.split('');
+  // test(splitData);
 
-  secondChild(element) {
-    let secondChildObj = {
-      type: 'Number',
-      value: element,
-      child: []
+  let child = [];
+  let arrKey = 0;
+  let strElement = '';
+
+  for (let key in splitData) {
+    const value = splitData[key];
+
+    if (value.match(/[0-9]/) && arrKey === 0) child.push(value);
+    if (value.match(/[0-9]/) && arrKey > 0) child[child.length -1].push(value);
+
+    if (value.match(/(\[)/g)) {
+      child.push(new Array());
+      const arrCondition = Object.prototype.toString.call(child[child.length - 1]);
+      if (arrCondition === '[object Array]') arrKey = child.length - 1;
     }
-    arr.push(secondChildObj);
-    return arr;
-  }
+    if (value === ',') { // 함수화 (',') 발견 시
+      if (strElement.match(/[0-9]/)) strElement = strElement + value;
+      strElement !== '' ? child[child.length - 1].push(strElement) : null;
+      strElement = '';
+    }
 
-  getCheckNumberType(wordData) {
-    return wordData.search(/(0-9)/) ? 'Number' : false;
-  }
-
-  arrParser(filterData) {
-    const splitStr = filterData.split('');
-    console.log(filterData)
-    console.log(splitStr)
-    let result = [];
-
-    for (let key in splitStr) {
-      let strElement = splitStr[key];
-
-      if(this.nextElement(strElement)) {
-        if(this.openArr(strElement)) result[result.length-1].push(strElement);
+    if (value.match(/(\])/g)) { // 함수화 (']') 발견시
+      return chlid;
+      strElement !== '' ? child[child.length - 1].push(strElement) : null;
+      strElement = child.pop();
+      if (key !== str.length - 1) {
+        child[child.length - 1].push(strElement);
+        strElement = '';
       }
-
-      if(this.isNumber(strElement)) result.push([strElement]);
-      // if(this.closeArr(strElement)) strElement = result.pop();
-      console.log(result);
     }
-  }
 
-  isNumber(element){
-    return element.match(/[0-9]/g);
-  }
 
-  openArr(element) {
-    return element === '[';
-  }
+    // if (arrKey !== str.length && value.match(/[0-9]/)) child[child.length - 1].push(value);
 
-  nextElement(element) {
-    return element === ',';
-  }
-
-  closeArr(element) {
-    return element === ']';
+    console.log(child);
+    console.log(arrKey, value);
   }
 }
 
-const str = '[1, 2,[3,4, 5]]';
-const result = new test();
-const filter = result.filterData(str);
-const arrParser = result.arrParser(filter);
-// result.secondChild(arrParser);
-console.log(JSON.stringify(result, null, 2));
+const testcase1 = '[1, 2,[3,4, 5]]';
+const testcase2 = '[12, [14, 55], 15]';
+const testcase3 = '[1, [55, 3]]';
+const testcase4 = '[1, [[2]]]';
+const testcase5 = '[123,[22,23,[11,[112233],112],55],33]';
+const testcase6 = '12345';
+const testcase7 = '[1,3,[1,2],4,[5,6]]';
+const result = parser(testcase1);
+// console.log(JSON.stringify(result, null, 2));
