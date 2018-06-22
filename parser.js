0bb7826c932a0f7326c57a4521b9e75a09ccaefb
feanar729 <feanar729@gmail.com>
2018-06-22 14:54:41 +0900

    refactoring & feedback & update: feedback 반영 및 기능 refactoring진행 / README 파일 STEP2 요구사항 추가

--- parser.js
+++ parser.js
@@ -1,51 +1,102 @@
 const commandMSG = {
   noneData: 'none Data',
   array: 'Array',
-  object: 'Object;',
-  blockError: 'BLOCK ERROR',
-  typeError: 'TYPE ERROR'
+  object: 'Object;'
 };
 
-// sublutin fun1 - getChildDataType
-function isArrayTypeValueChangeElement(wordElement) {
-  return wordElement.replace(/(\[|\])/g, '').trim();
-}
-// sublutin fun2 - getChildDataType
-function getCheckNumber(wordData) {
-  return wordData.search(/(0-9)/) ? 'Number' : false;
+const ERROR_MSG = {
+  BLOCK_ERROR: 'BLOCK ERROR',
+  TYPE_ERROR: 'TYPE ERROR'
+};
+
+// block error 확인
+function getCheckErrorBlock(arrWord) {
+  let arrBracket = 0;
+  const splitWord = arrWord.split('');
+
+  splitWord.forEach(matchArrVal => {
+    if (matchArrVal === '[') arrBracket++;
+    if (matchArrVal === ']') {
+      if (arrBracket === 0) throw new Error(ERROR_MSG.BLOCK_ERROR);
+      arrBracket--;
+    }
+  });
+
+  if (arrBracket === 0) {
+    let checkType = {
+      type: commandMSG.array,
+      child: getDeleteOfBlank(arrWord)
+    };
+    return checkType;
+  }
+  throw new Error(ERROR_MSG.BLOCK_ERROR);
 }
 
-// child 안의 child Data 값 확인 
-function getInChildDataType(inStringData) {
-  let stringWord = isArrayTypeValueChangeElement(inStringData);
-  let inChildData = {
-    type: 'Array',
-    value: 'ArrayObject',
-    child: [{
-      type: getCheckNumber(inStringData),
-      value: stringWord,
-      child: []
-    }]
-  }
-  return inChildData;
+// filtering Black in Arr Data
+function getDeleteOfBlank(word) {
+  const delOutsideArr = word.substring(1, word.length - 1);
+  const split = delOutsideArr.split('');
+
+  const result = split.filter(element => {
+    return element !== ' ';
+  }).reduce((accData, element) => {
+    return accData + element;
+  });
+
+  return getChildDataType(commandMSG.array, result);
+}
+
+// condition Number Check
+function isNumber(stringWord) {
+  if (stringWord.match(/[0-9]/)) return true;
+}
+
+// condition Array Check
+function isArray(stringWord) {
+  if (stringWord.match(/(\[|\])/)) return true;
 }
 
 // 데이터 형태 확인 및 데이터 값 반환 child 데이터의 유무 확인 
-function getChildDataType(typeCheck, wordArr) {
+function getChildDataType(typeCheck, stringWord) {
+  if (typeCheck !== 'Array') throw new Error(ERROR_MSG.TYPE_ERROR);
+
   if (typeCheck === 'Array') {
-    const splitData = wordArr.split(',');
+    const splitData = stringWord.split(',');
+    const test = stringWord.split('');
+    const filtering = test.filter(value => {
+      return value !== ',';
+    });
+
     let childArr = [];
 
-    for (let value in splitData) {
-      let stringWord = splitData[value];
-      if (stringWord.match(/(\[|\])/g)) {
-        const inChildData = getInChildDataType(stringWord);
-        childArr.push(inChildData);
-      } else if (stringWord.match(/[0-9]/)) {
+    let bracket = 0;
+    for (let key in filtering) {
+
+      if(filtering[key] === '['){
+        bracket++;
+      } else if(filtering[key] === ']'){
+        bracket--;
+      }
+      if(bracket !== 0){
+        childArr.push(filtering[key]);
+      } else {
+        console.log(filtering[key], "일반 데이터")
+      }
+    }
+    console.log(childArr)
+
+    for (let key in splitData) {
+      let stringWord = splitData[key];
+
+      if (isArray(stringWord)) {
+        const childOfChild = getChildOfChildDataType(stringWord);
+        childArr.push(childOfChild);
+
+      } else if (isNumber(stringWord)) {
         let childData = {
           type: getCheckNumber(stringWord),
           value: stringWord
-        }
+        };
         childArr.push(childData);
       }
     }
@@ -53,65 +104,60 @@ function getChildDataType(typeCheck, wordArr) {
   }
 }
 
-// block error 확인
-function getCheckErrorBlock(arrWord) {
-  let arrBracket = 0;
-  const splitWord = arrWord.split("");
-
-  splitWord.forEach(matchArrVal => {
-    if (matchArrVal === '[') arrBracket++;
-    if (matchArrVal === ']') arrBracket--;
-  });
-
-  if (arrBracket % 2 === 0) {
-    let checkType = {
-      type: commandMSG.array,
-      child: getChildDataType(commandMSG.array, arrWord)
-    }
-    return checkType;
-  }
-  throw new Error(commandMSG.blockError);
+// sublutin fun1 - getChildDataType
+function arrTypeValueChangeElement(wordElement) {
+  return wordElement.replace(/(\[|\])/g, '').trim();
 }
 
-// filtering Black in Arr Data
-function getDeleteInBlankData(word) {
-  const delOutsideArr = word.substring(1, word.length - 1);
-  const split = delOutsideArr.split("");
+// sublutin fun2 - getChildDataType
+function getCheckNumber(wordData) {
+  return wordData.search(/(0-9)/) ? 'Number' : false;
+}
 
-  const result = split.filter(element => {
-    return element !== " ";
-  }).reduce((accData, element) => {
-    return accData + element;
-  });
+// child 안의 child Data 값 확인
+function getChildOfChildDataType(inStringData) {
 
-  return result;
+  let stringWord = arrTypeValueChangeElement(inStringData);
+  const childArr = [];
+
+  const child = {
+    type: getCheckNumber(inStringData),
+    value: stringWord,
+    child: []
+  };
+  childArr.push(child);
+
+  let inChildData = {
+    type: 'Array',
+    value: 'ArrayObject',
+    child: childArr
+  };
+  return inChildData;
 }
 
 // 각각에 데이터함수에서 데이터 확인
 function ArrayParser(word) {
-  const delBlankArr = getDeleteInBlankData(word);
-  const typeCheck = getCheckErrorBlock(delBlankArr);
+  const checkArr = getCheckErrorBlock(word);
 
   const parsingData = {
-    type: typeCheck.type,
-    child: typeCheck.child
+    type: checkArr.type,
+    child: checkArr.child
   };
   return parsingData;
 }
 
-// const str = '[123, 22, 33, 4,[5,6],[7]]';
+
+const str = '[1,[2],[3,4,5]]';
 // const strArr = '[[3],123, 22, 33, 4,[5,6],7]';
 // const strArr2 = '[132132, 2321,32111,32942,3211,1,4,65,6,8,9,1,2,3,4,5,6,7,8,9]';
-const strArr3 = '[132132, [2321,32111],32942,[3211,1],4,65,6,8,9,1,2,[3,4],5,6,7,8,9]';
+// const strArr3 = '[132132, [2321,32111],32942,[3211,1],4,65,6,8,9,1,2,[3,4],5,6,7,8,9]';
 
-
-// const result = new ArrayParser(str);
+const result = new ArrayParser(str);
 // const resultFirst = new ArrayParser(strArr);
 // const resultSecond = new ArrayParser(strArr2);
-const resultThird = new ArrayParser(strArr3);
-
+// const resultThird = new ArrayParser(strArr3);
 
 // console.log(JSON.stringify(result, null, 2));
 // console.log(JSON.stringify(resultFirst, null, 2));
 // console.log(JSON.stringify(resultSecond, null, 2));
-console.log(JSON.stringify(resultThird, null, 2));
+// console.log(JSON.stringify(resultThird, null, 2));
