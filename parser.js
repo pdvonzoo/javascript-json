103da18754c3b7c02894ff9c3aea188b6edcf3bf
feanar729 <feanar729@gmail.com>
2018-07-20 15:39:23 +0900

    update & refactoring: 데이터 타입별 타입명 출력 기능 refactoring 진행

--- parser.js
+++ parser.js
@@ -4,7 +4,7 @@ const dataType = {
   arrayObj: 'Array Object',
   number: 'Number',
   string: 'String',
-  null: null,
+  null: null
 };
 
 const ERROR_MSG = {
@@ -12,45 +12,53 @@ const ERROR_MSG = {
   TYPE_ERROR: 'TYPE ERROR'
 };
 
-class DataStuctureObj {
+class DataTypeCheck {
   constructor(type, value, child) {
     this.type = type;
     this.value = value;
     this.child = child;
   }
 
-  getChild(data) {
-    this.childArr = [];
-    this.childArr.push(data);
-  }
+  // getChildOfChildType(value, child) {
+  //   let childOfChild = {
+  //     type: 'Array',
+  //     value: 'ArrayObject',
+  //     child: []
+  //   };
+  //   value.forEach(value => {
+  //     if (value.length === 0) return dataType.null;
+  //     childOfChild.child.push({
+  //       type: dataType.number,
+  //       value: value
+  //     });
+  //   });
+  //   return child.push(childOfChild);
+  // }
 
-  getChildOfChildObj(value) {
-    let child = [];
+  // getChildValue(value) {
+  //   if (value.length === 0) return dataType.null;
+  //   let childData = {
+  //     type: dataType.number,
+  //     value: value
+  //   };
+  //   return childData;
+  // }
 
-    if (value === undefined) return dataType.null;
-    child.push({
-      type: dataType.number,
-      value: value
-    });
-    return child;
-  }
+  // isArray(value) {
+  //   const isArray = Object.prototype.toString.call(value);
+  //   return isArray === '[object Array]';
+  // }
 
-  // child data Type 분류
-  getChildTokenType(parsing) {
-    let child = [];
+  // checkChildValType(parsingData) {
+  //   let child = [];
+  //   // console.log(parsingData)
 
-    for (let value of parsing) {
-      let isArray = Object.prototype.toString.call(value);
-      (isArray === '[object Array]') ? getChildOfChildObj(value, child): getChildObj(value, child);
-    }
-    return child;
-  }
+  //   for (let value of parsingData) {
+  //     (this.isArray(value)) ? this.getChildOfChildType(value, child): this.getChildValue(value);
+  //   }
+  //   return child;
+  // }
 }
-
-const test = new DataStuctureObj(dataType.array, dataType.arrayObj);
-
-console.log(test.getChild(new DataStuctureObj(dataType.array, dataType.arrayObj, test.getChildOfChildObj())));
-console.log(JSON.stringify(test.childArr, null, 2));
 
 function checkBlockError(arrWord) {
   let BracketPoint = 0;
@@ -74,17 +82,19 @@ function isOpenBrackets(value) {
   return openBrackets.indexOf(value) > -1;
 }
 
+function isCommaOrCloseBrackets(value) {
+  const closeBrackets = [']'];
+  return closeBrackets.indexOf(value) > -1 || value === ',';
+}
+
 function isCloseBrackets(value) {
   const closeBrackets = [']'];
   return closeBrackets.indexOf(value) > -1;
 }
 
-function isCommaToken(value) {
-  return value === ',';
-}
-
-// parsing 기능 
+// parsing 기능
 function stackData(strData) {
+  const checkType = new DataTypeCheck();
   const stackArr = [];
   let temp = '';
 
@@ -93,12 +103,10 @@ function stackData(strData) {
 
     if (isOpenBrackets(value)) {
       stackArr.push(new Array);
-    } else if (isCloseBrackets(value)) {
-      temp !== '' ? stackArr[stackArr.length - 1].push(temp) : null;
-      temp = stackArr.pop();
-    } else if (isCommaToken(value)) {
-      temp !== '' ? stackArr[stackArr.length - 1].push(temp) : null;
+    } else if (isCommaOrCloseBrackets(value)) {
+      temp !== '' ? stackArr[stackArr.length - 1].push(new DataTypeCheck(dataType.array, dataType.arrayObj, temp)) : null;
       temp = '';
+      if (isCloseBrackets(value)) temp = stackArr.pop();
     } else {
       temp = temp + value.trim();
     }
@@ -110,34 +118,40 @@ function parsingObj(strData) {
   const checkError = checkBlockError(strData);
 
   if (checkError === true) {
-    const result = stackData(strData);
-    return result;
+    const checkType = new DataTypeCheck();
+    const stack = stackData(strData);
+
+    const parsingResult = {
+      type: dataType.array,
+      // child: checkType.checkChildValType(stack),
+      child: stack
+    };
+    return parsingResult;
   }
 }
 
-// const testcase1 = '[11, 22,[3,41, 5]]';
-// const testcase2 = '[12, [14, 55], 15]';
-// const testcase3 = '[1, [55, [3]],[]]';
-// const testcase4 = '[1,3,[1,21,[2, 4324,[543, 432]]],324,[51,63],7]';
-// const testcase5 = '[[1123, 354445324328103829,[1, 2, [3],4,5,6]],[1,2],4,[5,6]]';
-// const testcase6 = '12345';
-// const testcase7 = '[[[]]]';
-// const testcase8 = '[[],[],4,[6,5,87],[78]]';
-// const testcase9 = '[[1],[2,3]]';
-// const testcase10 = '[11, [22], 33]';
-// const testcase11 = '[[[[1,[],2]],[]]]';
-// const testcase12 = '[1, [[2]]]';
-// const testcase13 = '[123,[22,23,[11,[112233],112],55],33]';
+const testcase1 = '[11, 22,[3,41, 5]]';
+const testcase2 = '[12, [14, 55], 15]';
+const testcase3 = '[1, [55, [3]],[]]';
+const testcase4 = '[1,3,[1,21,[2, 4324,[543, 432]]],324,[51,63],7]';
+const testcase5 = '[[1123, 354445324328103829,[1, 2, [3],4,5,6]],[1,2],4,[5,6]]';
+const testcase6 = '12345';
+const testcase7 = '[[[]]]';
+const testcase8 = '[[],[],4,[6,5,87],[78]]';
+const testcase9 = '[[1],[2,3]]';
+const testcase10 = '[11, [22], 33]';
+const testcase11 = '[[[[1,[],2]],[]]]';
+const testcase12 = '[1, [[2]]]';
+const testcase13 = '[123,[22,23,[11,[112233],112],55],33]';
 
 // const errorcase1 = '[3213, 2';
 // const errorcase2 = ']3213, 2[';
 // const errorcase3 = '[1, 55, 3]]';
 // const errorcase4 = '[[[p, []]]';
 
-
 // const test1 = parsingObj(testcase1);
 // const test2 = parsingObj(testcase2);
-// const test3 = parsingObj(testcase3);
+const test3 = parsingObj(testcase3);
 
 // const test4 = parsingObj(testcase4);
 // const test5 = parsingObj(testcase5);
@@ -152,9 +166,9 @@ function parsingObj(strData) {
 // const test12 = parsingObj(testcase12);
 // const test13 = parsingObj(testcase13);
 
-// const errorTest1 = parsingObj(errorcase1); 
-// const errorTest2 = parsingObj(errorcase2); 
-// const errorTest3 = parsingObj(errorcase3); 
-// const errorTest4 = parsingObj(errorcase4); 
+// const errorTest1 = parsingObj(errorcase1);
+// const errorTest2 = parsingObj(errorcase2);
+// const errorTest3 = parsingObj(errorcase3);
+// const errorTest4 = parsingObj(errorcase4);
 
-// console.log(JSON.stringify(test4, null, 2));
+console.log(JSON.stringify(test3, null, 2));
