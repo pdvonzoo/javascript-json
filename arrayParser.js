57f84de22654e9fc594c0d92938803f61991777e
Vanary <boyoung@vanary.com>
2018-10-16 10:55:05 +0900

    [Step4] Expand support for multiple element data type (#81)
    * Update method to use object literal instead of multiple IFs
    
    * Resolve "this" relationship between object rules / arrLexer
    
    * Update string lexer to handle string element in arrStr
    
    * Expand supporting element data types to string, boolean, null object
    
    * Update error message for non-string data with mixed number & string
    
    * Expand character processing support & error logging for elements in invalid data format
    
    Fix bug on whiteSpace / mixed string processing
    
    Update parser to handle special character input for string stream
    
    Update parser to return error on invalid keyword element
    
    Update test cases
    
    Fix issue on processing parenthesis in string stream
    
    Added error case for whiteSpace during number type data input
    
    Update test cases
    
    * Add individual method for logging error message
    
    * Assign right names for methods following there data structure
    
    * Add support for trailing whiteSpaces after lexeme
    
    * Improve readability of updateItem method
    
    * Update some comments
    
    * Improve readability of stringInput method
    
    * Update methods in Object literal to ES2015 template
    
    * Update way of calling other parser methods
    
    * Improve long condition phrase into individual function
    
    * Fix if statement error on whiteSpace processing method
    
    * Improve string/keyword lexeme handling by removing unnecessary stack operation
    
    * Improve token processing methods to have return value
    
    * Improve processing rule for last array element

--- arrayParser.js
+++ arrayParser.js
@@ -7,67 +7,40 @@ function arrayParser(arrStr) {
 
 const arrLexer = {
     tempMemory: [],
-    dataBranchQueue: [],
+    dataBranchStack: [],
     dataTree: [],
     lexer(arrStr) {
         // create array data branch & add chilren information
         arrStr.split('').forEach( (token) => { 
-            const tokenType = this.tagTokenType(token);
-
-            rules.charProcessing.array[tokenType].call(this, token);
+            const tokenDataObj = {token: token, stack: this.dataBranchStack, memory: this.tempMemory};
+            const tokenType = rules.tagTokenType(token);
+            // if incoming string is not predefined, process it as string token
+            if(!rules.charProcessing.array[tokenType]) { 
+                rules.process('string', tokenDataObj, 'strToken');
+                return
+            }
+            rules.process('array', tokenDataObj, tokenType);
         });
         
         this.dataTree.push(this.tempMemory.pop());
         
         return this.dataTree.pop();
     },
-    tagTokenType(token) {
-        let tokenType = token;
-        
-        if(token.match(/[0-9]/)) {
-            tokenType = 'number';
-        } else if (token.match(/\s/)) {
-            tokenType = 'whiteSpace';
-        } else if (token.match(/,/)) {
-            tokenType = 'updateItem';
-        }
-
-        return tokenType
-    },
 };
 
 const rules = {
-    charProcessing: {
-        array: {
-            '[': function () { //open new data branch
-                const newArrayTree = {type: 'array', child: []};
-                this.dataBranchQueue.push(newArrayTree);
-            },
-            'number': function(token) { // append or update last child object on temporary memory
-                const currentTempItem = this.tempMemory.pop();
-                const updatedTempItem = rules.updateLexeme('number', token, currentTempItem);
-                
-                this.tempMemory.push(updatedTempItem);
-            },
-            'updateItem': function() { // append child object on temporary memory to parent array
-                const currentDataBranch = rules.getLastItemOfArr(this.dataBranchQueue);
-                const childToAdd = rules.updateItemValue( this.tempMemory.pop() );
-                
-                currentDataBranch.child.push(childToAdd);
-            },
-            'whiteSpace': () => undefined, // do nothing
-            ']': function() { // append last child object on temporary memory. Close data branch
-                rules.charProcessing.array.updateItem.call(this);
-                
-                const arrayLexeme = this.dataBranchQueue.pop();
-                this.tempMemory.push(arrayLexeme);
-            },
-        },
+    process(dataType, {token, stack, memory}, tokenType = this.tagTokenType(token)) {
+        // Call proper token processing method following submitted dataType info
+        const [targetLocation, objToPush] = this.charProcessing[dataType][tokenType](arguments[1]);
+        
+        if(objToPush) {
+            targetLocation.push(objToPush);
+        }
     },
     getLastItemOfArr(arr) {
         return arr[arr.length-1]
     },
-    updateLexeme(type, token, tempItem) {
+    concatLexeme(type, token, tempItem) {
         if (!tempItem){
             tempItem = {type: type, value: token};
         } else {
@@ -77,25 +50,207 @@ const rules = {
         return tempItem
     },
     updateItemValue(dataObj) {
-        if (!dataObj) { 
-            dataObj = {type: 'undefined', value: undefined};
-        } else if(dataObj.type !== 'object' && dataObj.type !== 'array') { 
-            dataObj = Object.assign( dataObj, {value: this.assignDataType(dataObj)} );
-        } else if(dataObj.type === 'array') {
-            dataObj = Object.assign( dataObj, {value: 'arrayObject'} );
-        }
+        const updateRule = {
+            noObj: () => ( {type: 'undefined', value: undefined} ),
+            array: () => Object.assign( dataObj, {value: 'arrayObject'} ),
+            number: () => {
+                const updatedValueWithType = this.assignDataType(dataObj);
+                if(isNaN(updatedValueWithType)) {
+                    // Log error message if data branch has other characters than number strings
+                    logError(`${dataObj.value} : 알 수 없는 타입입니다!`);
+                }
 
-        return dataObj
-    },
-    assignDataType({type: targetType, value}) {
-        const processingRulesTo = {
-            'number': function(value) {
-                return Number(value)
+                return Object.assign( dataObj, {value: updatedValueWithType} )
+            },
+            string: () => dataObj, // Do nothing
+            openString: () => Object.assign( dataObj, {type: 'string'} ), 
+            keyword: () => {
+                const keywordObj = rules.charProcessing.keyword.dictionary[dataObj.value];
+                
+                // If given keyword lexeme doesn't exist on dictionary, log error
+                if (!keywordObj) { 
+                    logError(`${dataObj.value} : 존재하지 않는 명령어입니다!`);
+                }
+
+                return keywordObj
+            },
+            errorString: () => {
+                logError(`${dataObj.value} : 올바른 문자열이 아닙니다!`);
             },
         };
-        return processingRulesTo[targetType](value)
+        
+        const dataType = (dataObj) ? dataObj.type : 'noObj';
+
+        return updateRule[dataType]()
+    },
+    assignDataType({type: targetType, value}) {
+        const convertTypeTo = {
+            'number': (value) => Number(value),
+        };
+        return convertTypeTo[targetType](value)
+    },
+    adjustStack(dataObj, stack) {
+        const typesRequireStackAdjustment = {
+            'keyword': true,
+            'errorString': true,
+        }
+        if (typesRequireStackAdjustment[dataObj.type]) {
+            stack.length--;
+        }
+    },
+    tagTokenType(token) {
+        let tokenType = token;
+
+        if(token.match(/[0-9]/)) {
+            tokenType = 'number';
+        } else if (token.match(/\s/)) {
+            tokenType = 'whiteSpace';
+        } else if (token.match(/,/)) {
+            tokenType = 'appendElem';
+        } else if (token.match(/['"`]/)) {
+            tokenType = 'stringInput';
+        } else if (token.match(/[a-zA-Z]/)) {
+            tokenType = 'string';
+        } else if (token.match(/\[/)) {
+            tokenType = 'arrayOpen';
+        } else if (token.match(/\]/)) {
+            tokenType = 'arrayClose';
+        }
+
+        return tokenType
+    },
+    removeAdditionalWhiteSpace(string) {
+        return string.slice(0, string.match(/\S\s*$/).index + 1)
     },
 };
+
+/* ============================
+    Token processing rules 
+=============================== */
+rules.charProcessing = {};
+rules.charProcessing.array = {
+    arrayOpen({token, stack, memory}) { //open new data branch
+        if(memory[0] && memory[0].type === 'openString') {
+            return rules.charProcessing.string.strToken({token, stack, memory})
+        }
+
+        const newArrayTree = {type: 'array', child: []};
+        return [stack, newArrayTree]
+    },
+    number({token, memory}) { // append or update last child object on temporary memory
+        const currentTempItem = memory.pop();
+        const updatedTempItem = rules.concatLexeme('number', token, currentTempItem);
+        
+        return [memory, updatedTempItem]
+    },
+    stringInput({token, stack, memory}) {
+        return rules.charProcessing.string.stringInput({token, stack, memory})
+    },
+    string({token, stack, memory}) {
+        // If string token appears out of nowhere, process it as opening token for keyword stream 
+        if (!memory[0]) {
+            return rules.charProcessing.keyword.keywordInput({token, stack, memory});
+        }
+        return rules.charProcessing.string.strToken({token, stack, memory})
+    },
+    appendElem({token, stack, memory}) { // Append item in memory to parent array
+        // if current stream is on string element, process token as pure string
+        if(memory[0] && memory[0].type === 'openString') {
+            return rules.charProcessing.string.strToken({token, stack, memory})
+        }
+        
+        const itemInMemory = memory.pop();
+        const currentDataBranch = rules.getLastItemOfArr(stack);
+        // if item to update is keyword / string / number, remove all trailing whitespaces
+        if (itemInMemory && itemInMemory.value) {
+            itemInMemory.value = rules.removeAdditionalWhiteSpace(itemInMemory.value);
+        }
+
+        const childToAdd = rules.updateItemValue(itemInMemory);
+        
+        return [currentDataBranch.child, childToAdd]
+    },
+    whiteSpace({token, stack, memory}) {
+        // if current stream is object or array element, do nothing
+        const isNotObjectNorArray = ( dataObj ) => {
+            if (dataObj) {
+                return (dataObj.type !== 'object' || dataObj.type !== 'array') ? true : false
+            }
+            return false
+        };
+        
+        if( isNotObjectNorArray(memory[0]) ) { 
+            return rules.charProcessing.string.strToken({token, stack, memory});
+        }
+        
+        return []// Nothing will happen
+    }, 
+    arrayClose({token, stack, memory}) { // Append last child object on temporary memory. And then close data branch
+        // if current stream is on string element, work as normal token
+        if(memory[0] && memory[0].type === 'openString') { 
+            return rules.charProcessing.string.strToken({token, stack, memory});
+        }
+
+        if(memory[0]) { // append leftover element if exists
+            rules.process('array', {token, stack, memory}, 'appendElem');
+        }
+        
+        const arrayLexeme = stack.pop();
+        return [memory, arrayLexeme]
+    },
+};
+rules.charProcessing.string = {
+    stringInput ({token, stack, memory}) { // Open new data branch if there is no current one. If it exists, close it.
+        // if there are ongoing string stream on stack, close it
+        if ( memory[0] && memory[0].type === 'openString' ) {
+            const concatedToken = rules.concatLexeme('string', token, memory.pop());
+            return [memory, rules.updateItemValue(concatedToken)];
+        }
+
+        // If there are string stream left on memory and yet this function was called, 
+        // assign current lexeme as errorString to log error later when update lexeme to parent array
+        const newStrTree = ( () => {
+            if (memory[0] && memory[0].type === 'string') {
+                return {type: 'errorString', value: memory.pop().value + token}
+            }
+             return {type: 'openString', value: token}
+        }) ();
+
+        return [memory, newStrTree]
+    },
+    strToken({token, memory}) { // append or update last child object on temporary memory
+        const currentTempItem = memory.pop();
+        const updatedTempItem = rules.concatLexeme('string', token, currentTempItem);
+        
+        return [memory, updatedTempItem]
+    },
+};
+
+rules.charProcessing.keyword = {
+    keywordInput ({token, stack, memory}) { //open new data branch
+        const newKeywordTree = {type: 'keyword', value: token};
+        
+        return [memory, newKeywordTree]
+    },
+    dictionary: {
+        'true': {type: 'boolean', value: true},
+        'false': {type: 'boolean', value: false},
+        'null': {type: 'object', value: null},
+    },
+};
+
+
+function logError(msgStr) {
+    try {
+        throw msgStr
+    }
+    catch(e) {
+        console.log(e);
+    }
+}
+
+
+
 
 // Export to tester.js 
 module.exports.arrayParser = arrayParser;
--- tester.js
+++ tester.js
@@ -1,79 +1,32 @@
 // import arrayParser.js through Node.js
 const main = require('./arrayParser');
 
+console.log(`\n========== 정상 시나리오 1 ==========\n`);
+var s = "['ab,12',[2580,['bbc4',5],null]       , [true, false, 'kbs2'], 595,]";
+var result = main.arrayParser(s);
+console.log(JSON.stringify(result, null, 2)); 
 
+console.log(`\n========== 정상 시나리오 2 - 문자열 내 특수문자 처리 ==========\n`);
+var s = "[',[]{}!@#$%^&*(1a3',[null,false,['11',[112233]],55, '99'],33, ,true]";
+var result = main.arrayParser(s);
+console.log(JSON.stringify(result, null, 2)); 
 
-const str = "[488, [2573, [48, [1577, 1577]], 369], 13, [1,2,3]]";
-const result = main.arrayParser(str);
-console.log(JSON.stringify(result, null, 4));
+console.log(`\n========== 오류 시나리오 1 : 잘못된 문자열 ==========\n`);
+var s = "['12She's gone',[22,23,[11,[112233],112],55],33]";
+var result = main.arrayParser(s);
+// '12She's gone' : 올바른 문자열이 아닙니다!
 
-// Output
-// {
-//     "type": "array",
-//     "child": [
-//         {
-//             "type": "number",
-//             "value": 488
-//         },
-//         {
-//             "type": "array",
-//             "child": [
-//                 {
-//                     "type": "number",
-//                     "value": 2573
-//                 },
-//                 {
-//                     "type": "array",
-//                     "child": [
-//                         {
-//                             "type": "number",
-//                             "value": 48
-//                         },
-//                         {
-//                             "type": "array",
-//                             "child": [
-//                                 {
-//                                     "type": "number",
-//                                     "value": 1577
-//                                 },
-//                                 {
-//                                     "type": "number",
-//                                     "value": 1577
-//                                 }
-//                             ],
-//                             "value": "arrayObject"
-//                         }
-//                     ],
-//                     "value": "arrayObject"
-//                 },
-//                 {
-//                     "type": "number",
-//                     "value": 369
-//                 }
-//             ],
-//             "value": "arrayObject"
-//         },
-//         {
-//             "type": "number",
-//             "value": 13
-//         },
-//         {
-//             "type": "array",
-//             "child": [
-//                 {
-//                     "type": "number",
-//                     "value": 1
-//                 },
-//                 {
-//                     "type": "number",
-//                     "value": 2
-//                 },
-//                 {
-//                     "type": "number",
-//                     "value": 3
-//                 }
-//             ],
-//             "value": "arrayObject"
-//         }
-//     ]
-// }
+console.log(`\n========== 오류 시나리오 2 : 알 수 없는 자료형 ==========\n`);
+var s = "['1a,3',[22,23,[11,[112233],112],55],3%^&*3]";
+var result = main.arrayParser(s);
+// 3%^&*3 : 알 수 없는 타입입니다!
+ 
+console.log(`\n========== 오류 시나리오 2 - 1 : 알 수 없는 키워드 ==========\n`);
+var s = "['1a,3',[22,23,[11,[112233],112],55], true thy ]";
+var result = main.arrayParser(s);
+// true thy : 존재하지 않는 명령어입니다!
+
+console.log(`\n========== 오류 시나리오 2 - 2 : 숫자열 안의 공백문자 처리 ==========\n`);
+var s = "['1a,3',[22,23,[11,[112  233],112],55], true    ]";
+var result = main.arrayParser(s);
+// 112  233 : 알 수 없는 타입입니다!
