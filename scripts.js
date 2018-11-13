5692ec18a3436488b5f3168c5de7d310367f5df2
pdvonzoo <vonzoo@naver.com>
2018-11-13 18:28:51 +0900

    Change some declaration of a variable to const (#108)

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ class Parser{
     this.root = [];
   }
   processData(data){
-    let target = 0, curr = this.root, child, parent, value;
+    let target = 0, value, child, parent, curr = this.root;
     for(let v of data){
       target++;
       if(v === '[' || v === ']' || v === ','){
@@ -17,42 +17,39 @@ class Parser{
     return this.root;
   }
   getvalue(data, target){
-    let openingBracket = data.indexOf('[', target),
-        closingBracket = data.indexOf(']', target),
-        rest = data.indexOf(',', target),
-        compareValues = [openingBracket, closingBracket, rest];
+    const openingBracket = data.indexOf('[', target),
+          closingBracket = data.indexOf(']', target),
+          rest = data.indexOf(',', target),
+          compareValues = [openingBracket, closingBracket, rest];
     
-    let stopPoint = compareValues.reduce((acc, curr) => {
+    const stopPoint = compareValues.reduce((acc, curr) => {
       if(curr === -1) return acc;
       if(acc === -1 || acc > curr) return curr;
       return acc;
-    })
+    })    
     if(stopPoint === -1) return;
     return data.substring(target, stopPoint);
   }
   setValue(value, curr, child, parent, input){
-    let trimValue = this.trimData(value);
-    let type =  this.checkType(value);
+    const trimValue = this.trimData(value),
+          type =  this.checkType(value);
 
     if(input  === '['){
       curr.push({ type: 'array', child: [], parent: parent, });
       child = curr.slice(-1)[0].child;
       parent = curr;
     }
-    if(input  === ']'){
-      parent = curr.slice(-1)[0].parent;
-      child = parent;
-    }
+    if(input  === ']')   child = curr.slice(-1)[0].parent;
     if(trimValue !== '') child.push({ value: trimValue, type,  child: [], parent: parent, });
-    if(input  === '[') curr = child;
-    if(input  === ']') curr = parent;
+    if(input  === '[' || input  === ']') curr = child;
 
     return [curr, child, parent];
   }
   trimData(data){
-    let convert = data.split(",");
-    let trimmedData = convert.map(v => v.trim()).filter(v => v !== '').join(',');
-	  return trimmedData;
+    return  data.split("")
+                .map(v => v.trim())
+                .filter(v => v !== "")
+                .join("");
   }
   checkType(data){
     if(toString.call(data) === "[object Object]") return 'object';
@@ -68,5 +65,5 @@ function replacer(key, value){
 }
 
 const parser = new Parser();
-let result = parser.processData("[ 123,14, 56, 55]");
+const result = parser.processData("[[42,65] 123,14, [52, 98], 56, [55, 41]]");
 console.log(JSON.stringify(result, replacer, 2));
