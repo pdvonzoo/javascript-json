4c2e924dc0f12a0400b3f5cbf80b33991bbd4116
Kim <a01088570194@gmail.com>
2018-11-20 22:32:20 +0900

    Remove child variable and Refactoring

--- dataType.js
+++ dataType.js
@@ -5,12 +5,11 @@ class DataType{
     check(data){
         if(data === '') return;
         if(this.isBoolean(data)) return 'boolean';
-        if(this.isNumber(data)) return 'number';
         if(this.isNull(data)) return 'null';
+        if(this.isNumber(data)) return 'number';
         if(this.isString(data)) {
             if(!this.error.validateString(data)) return 'string';            
         }
-        this.error.validateOther(data);
     }
     isString(value){
         return value.includes("'") | value.includes('"');
@@ -22,21 +21,14 @@ class DataType{
         return value === 'null';
     }
     isNumber(value){
-        return !isNaN(value);
+        return value.match(/^[0-9]*$/);
     }
 }
 
 class Error{
     validateString(value){
-        // if(!value.match(/^\'/)) throw "문자열의 시작은 '이어야 합니다.";
-        // if(!value.match(/\'$/)) throw "문자열의 끝은 '이어야 합니다.";
-        // if((value.match(/\'/g) || []).length > 2) throw "문자열 안에 '이 포함되지 않아야 합니다.";
-        return false;
-    }
-    validateOther(value){
-        // if(value.match(/[a-z][0-9]/gi)) throw "숫자 안에 문자가 들어갈 수 없습니다.";
-        // throw "잘못된 문자열입니다.";
-        return false;
+        if(!value.match(/^\'/)) throw "문자열의 시작은 '이어야 합니다.";
+        if(!value.match(/\'$/)) throw "문자열의 끝은 '이어야 합니다.";
     }
 }
 
