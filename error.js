25c41b2d3a8c1cfe77e7be6631f9cbab0d4085cd
Kim <a01088570194@gmail.com>
2018-11-26 21:08:38 +0900

    Add validate for additional situation

--- error.js
+++ error.js
@@ -12,8 +12,10 @@ class ErrorChecker{
             if(v === ']') ++closingArr;
             if(v === '\'') ++StrSpecialChar;
         }
-        if(openingObj !== closingObj) throw "올바르지 않은 객체가 포함되어 있습니다.";
-        if(openingArr !== closingArr) throw "올바르지 않은 배열이 포함되어 있습니다.";
+        if(openingObj > closingObj) throw "정상적으로 종료되지 않은 객체가 포함되어 있습니다.";
+        if(openingObj < closingObj) throw "객체의 종료가 초과하여 일어났습니다.";
+        if(openingArr > closingArr) throw "정상적으로 종료되지 않은 객체가 있습니다.";
+        if(openingArr < closingArr) throw "배열의 종료가 초과하여 일어났습니다.";
         if(StrSpecialChar % 2 !== 0) throw "올바르지 않은 문자열이 포함되어 있습니다.";
     }
     validateColon(data){
